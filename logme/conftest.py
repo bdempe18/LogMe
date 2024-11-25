f13379840bc80ac15d106fa5e70f6d9192668d1e
bedempe18 <dempeb18@gmail.com>
2024-11-25 10:54:36 -0600

    init

---
+++ logme/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from logme.users.models import User
+from logme.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
